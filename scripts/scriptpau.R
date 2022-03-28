47f4fa7f8f93f1fa0000ad58257fd4d62c3a9731
paula <pmachenbach@gmail.com>
2022-03-28 01:57:36 -0300

    van los comienzos del variograma

--- scripts/scriptpau.R
+++ scripts/scriptpau.R
@@ -3,33 +3,49 @@ library(gstat)
 library(geoR)
 library(spdep)
 
-AKN.pntXY@coords = (AKN.pntXY@coords)/1e4
-#unique(AKN.pntXY@data$Observation.Date)
+d = sp_data
+#latitude, longitude y temp
+class(d)
 
-AKN.pntXY2 = AKN.pntXY[AKN.pntXY@data$Observation.Date == "2006-05-09",]
-plot(AKN.pntXY2)
+coordinates(d) = ~longitude+latitude
+summary(d)
+#quisiera tener el distance summary
 
-v_nube <- variogram(Dens~1, AKN.pntXY2, cloud=T)
-plot(v_nube)
+#vemos si tiene tendencia
+gd<-as.geodata(d)
+class(gd)
+plot(gd, lowess=TRUE)
+#tendencia negativa en y (latitude)
 
-v_map <- variogram(Dens~1, AKN.pntXY2, cutoff=80, width = 10, map=T)
-plot(v_map)
+#variograma sin tendencia
+v_nube_sintend <- variogram(temp~1, d, cloud=T)
+plot(v_nube_sintend)
+v_map_sintend <- variogram(temp~1, d, cutoff=4, width = 1, map=T)
+plot(v_map_sintend)
 
-
-#df = as.data.frame(AKN.pntXY)
-#class(df)
-#colnames(df)
-
-#df$Latitude = df$Latitude/1e4
-#df$Longitude = df$Longitude/1e4
-
-#min(df$Longitude)
-#min(AKN.pntXY@coords[,1])
-
-#datosg<-as.geodata(AKN.pntXY2)
-#plot(datosg)
-
-#4267
+#variograma con tendencia en y
+v_nube_tend_y <- variogram(temp~latitude, d, cloud=T)
+plot(v_nube_tend_y)
+v_map_tend_y <- variogram(temp~latitude, d, cutoff=4, width = 1, map=T)
+plot(v_map_tend_y)
+#aun agregando la tendencia no queda lindo
+#ver más parámetros para variogram?
 
 #data_clean <- filter(df, Latitude>-83.98&Latitude<-79.51,Longitude>24.98&Longitude<31.55)
-#esto me dice que no encuentra longitude nideasds
+
+#ajusto variograma teórico
+v = variogram(temp~latitude, d)
+plot(v)
+
+vt_exp = fit.variogram(v, vgm(2.5, "Exp", 4, 1))
+vt_exp
+plot(v, vt_exp)
+
+vt_sph = fit.variogram(v, vgm(2.5, "Sph", 4, 1))
+vt_sph
+plot(v , vt_sph)
+#creo que este me gusta mas
+
+attr(vt_exp, 'SSErr')
+attr(vt_sph, 'SSErr')
+#tiene menos error también
