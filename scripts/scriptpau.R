361885f7942ae7ea68224d947b4e340845b706fd
paula <pmachenbach@gmail.com>
2022-03-29 23:48:33 -0300

    Agrego más variogramas, todo muy inconcluso igual todavía

--- scripts/scriptpau.R
+++ scripts/scriptpau.R
@@ -1,4 +1,3 @@
-##Script PAJARITOS
 library(gstat)
 library(geoR)
 library(spdep)
@@ -23,9 +22,38 @@ plot(v_nube_sintend)
 v_map_sintend <- variogram(temp~1, d, cutoff=4, width = 1, map=T)
 plot(v_map_sintend)
 
-## variograma empirico
+#variograma empirico
+nube_clasica <- variog(gd, option = "cloud")
+plot(nube_clasica)
+
+nube_CH <- variog(gd, option = "cloud", estimator.type = "modulus")
+plot(nube_CH)
+
 bin_clasico <- variog(gd, uvec=seq(0,8,l=18))
 plot(bin_clasico)
+
+bin_CH <- variog(gd, uvec=seq(0,8,l=18), estimator.type= "modulus")
+plot(bin_CH)
+
+bin1 <- variog(gd, uvec = seq(0,8,l=18), bin.cloud = T)
+bin2 <- variog(gd, uvec = seq(0,8,l=18), estimator.type = "modulus", bin.cloud = T)
+
+plot(bin1, bin.cloud = T, main = "classical estimator")
+plot(bin2, bin.cloud = T, main = "modulus estimator")
+
+vario.4 <- variog4(gd, max.dist = 5)
+
+plot(vario.4, lwd = 2)
+
+#vario.2 <- variog(elevation, uvec = seq(0,5,l=11), dir=0)
+#vario.3 <- variog(elevation, uvec = seq(0,5,l=11), dir=pi/2)
+#vario.4 <- variog(elevation, uvec=seq(0,5,l=11), dir=pi/4)
+
+#plot(bin_clasico, type="l")
+#lines(vario.2, lty = 2, col = 2)
+#lines(vario.3, lty = 3, col = 3)
+#lines(vario.4, lty = 4, col = 4)
+#legend("topleft", c("omnidireccional", "0", "90", "45"), col=c(1,2,3,4), lty=c(1,2,3,4))
 
 
 #variograma con tendencia en y
