5bf7c624b114381c0fbae909b310d7ada797c824
Nahuel Pelli <nahuelpelli91@gmail.com>
2022-04-02 14:23:51 -0300

    Agrego cambios script pau

--- scripts/scriptpau.R
+++ scripts/scriptpau.R
@@ -41,7 +41,7 @@ bin2 <- variog(gd, uvec = seq(0,8,l=18), estimator.type = "modulus", bin.cloud =
 plot(bin1, bin.cloud = T, main = "classical estimator")
 plot(bin2, bin.cloud = T, main = "modulus estimator")
 
-vario.4 <- variog4(gd, max.dist = 5)
+vario.4 <- variog4(gd, max.dist = 12)
 
 plot(vario.4, lwd = 2)
 
@@ -59,14 +59,16 @@ plot(vario.4, lwd = 2)
 #variograma con tendencia en y
 v_nube_tend_y <- variogram(temp~latitude, d, cloud=T)
 plot(v_nube_tend_y)
-v_map_tend_y <- variogram(temp~latitude, d, cutoff=1, width = 1, map=T)
+v_map_tend_y <- variogram(temp~latitude, d, cutoff = 4, width = 1, map=T)
+plot(v_map_tend_y)
+v_map_tend_y <- variogram(temp~latitude+longitude, d, cutoff = 4, width = 1, map=T)
 plot(v_map_tend_y)
 #aun agregando la tendencia no queda lindo
 #ver más parámetros para variogram?
 
 ## variograma empirico
-bin_clasico <- variog(temp~latitude, uvec=seq(0,8,l=18))
-plot(bin_clasico)
+vg_trend <- variog(gd, trend = "1st", uvec = seq(0,8,l = 16))
+plot(vg_trend)
 
 
 #data_clean <- filter(df, Latitude>-83.98&Latitude<-79.51,Longitude>24.98&Longitude<31.55)
@@ -75,11 +77,14 @@ plot(bin_clasico)
 v = variogram(temp~latitude, d)
 plot(v)
 
-vt_exp = fit.variogram(v, vgm(2.5, "Exp", 4, 1))
+vgm(3, "Exp", 8,1)
+
+
+vt_exp = fit.variogram(v, vgm(3, "Exp", 1,1))
 vt_exp
 plot(v, vt_exp)
 
-vt_sph = fit.variogram(v, vgm(2.5, "Sph", 4, 1))
+vt_sph = fit.variogram(v, vgm(2.5, "Sph", 1, 1))
 vt_sph
 plot(v , vt_sph)
 #creo que este me gusta mas
@@ -87,3 +92,6 @@ plot(v , vt_sph)
 attr(vt_exp, 'SSErr')
 attr(vt_sph, 'SSErr')
 #tiene menos error también
+
+
+patos = eyefit(vg_trend)
